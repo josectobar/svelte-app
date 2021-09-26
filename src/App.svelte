e0bd063440c0ef658f21870a1fabea43a3251125
Jose Tobar <josectobar@gmail.com>
2021-09-25 22:48:45 -0700

    installed deps and added svg

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
-	let count = 0
-	
-	function increment() {
-		count +=1
-	}
+  import blob from "./assets/images/blobs.svg";
+  //   const blobsEncoded = `data:image/svg+xml,${encodeURIComponent(blobs)}`;
+  let count = 0;
+
+  function increment(): void {
+    count += 1;
+  }
+  console.log("hit");
 </script>
 
 <h1>Animated Buttons</h1>
 <p>{count}</p>
-<button on:click={increment}>
-	Click here!
-</button>
+<button on:click={increment}> Click here! </button>
 
-<button on:click={() => count = 0}>
-	Reset
-</button>
+<button on:click={() => (count = 0)}> Reset </button>
+{blob}
+
+<style>
+  #blobC {
+    fill: red;
+  }
+</style>
