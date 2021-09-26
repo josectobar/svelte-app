717187f43583a353b01f8bc38706484b7d158bec
Jose Tobar <josectobar@gmail.com>
2021-09-25 21:36:05 -0700

    converted to TS

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let count = 0
+	
+	function increment() {
+		count +=1
+	}
+</script>
+
+<h1>Animated Buttons</h1>
+<p>{count}</p>
+<button on:click={increment}>
+	Click here!
+</button>
+
+<button on:click={() => count = 0}>
+	Reset
+</button>
